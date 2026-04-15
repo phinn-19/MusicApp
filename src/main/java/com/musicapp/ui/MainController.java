c6efc7438e019ecbb6892c7be8e5c16ad403508b
phinn-19 <phinn-19@users.noreply.github.com>
2026-04-15 18:56:31 +0700

    Fix: Add missing HBox import in MainController

--- src/main/java/com/musicapp/ui/MainController.java
+++ src/main/java/com/musicapp/ui/MainController.java
@@ -16,6 +16,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
